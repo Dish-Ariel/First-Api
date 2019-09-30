69beff45c2f4c109411aa418675c872ec727241b
Ariel Andrade <ariel.andrade@dish.com.mx>
2019-09-30 08:21:40 -0500

    cambio de el archivo de heroku

--- app.py
+++ app.py
@@ -6,5 +6,15 @@ def hello(name: hug.types.text, age: hug.types.number, hug_timer=3):
 
 @hug.get('/')
 def root():
-    return {'mensaje':'Bienvenido a mi Api prruki!','posdata':'accede a hello/name=<tu_nombre>&age=<tu_edad>','remitente':'Ariel Andrade Garcia V:'}
+    return {'mensaje':'Bienvenido a mi Api!!!','posdata':'accede a hello/name=<tu_nombre>&age=<tu_edad>','remitente':'Ariel Andrade Garcia V:'}
+
+@hug.default_input_format("application/json")
+@hug.post('/')
+def postRoot():
+    return {'mensaje':'Bienvenido a mi Api-post prruki!','posdata':'accede a hello/name=<tu_nombre>&age=<tu_edad>','remitente':'Ariel Andrade Garcia V:'}
+
+@hug.default_input_format("application/json")
+@hug.get('/customer/')
+def test(customer_id: hug.types.text):
+    return {'customer_id':'{0}'.format(customer_id), 'Name':'Nombre','Info':'Datos'}
 
